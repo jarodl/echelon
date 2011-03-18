61930b6d8a292faca6079513eaf173a7db365d66
Jarod Luebbert <jarodluebbert@gmail.com>
2011-03-18 01:26:41 -0500

    Added other models to django admin

--- echelon/admin.py
+++ echelon/admin.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib import admin
 
-from echelon.models import Category, Page
+from echelon.models import Category, Page, SiteSettings, SiteVariable
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'desc', 'content',)
@@ -10,5 +10,13 @@ class PageAdmin(admin.ModelAdmin):
     filter_list = ('categories',)
     list_display = ('title', 'content', 'script',)
 
+class SiteSettingsAdmin(admin.ModelAdmin):
+    filter_list = ('global_javascript',)
+
+class SiteVariableAdmin(admin.ModelAdmin):
+    filter_list = ('name', 'value',)
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(SiteSettings, SiteSettingsAdmin)
+admin.site.register(SiteVariable, SiteVariableAdmin)
